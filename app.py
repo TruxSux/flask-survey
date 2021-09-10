3b17846cfc4c5c7917dfa41880eb717e8e09cfd7
Trux <kaithomasdean@outlook.com>
2021-09-09 18:48:55 -1000

    Added flask session storage for current question

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
-import flask
+from flask import render_template, session, redirect, request, Flask, flash
 import surveys
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 app.config["SECRET_KEY"] = 'nimdA'
-responses = []
-c_qid = 0
-
 
 @app.route("/")
 def show_home_page():
-    global c_qid
-    global responses
-    c_qid = 0
-    responses = []
-    return flask.render_template("home.html")
-
+    if not session.get('c_qid'):
+        session["c_qid"] = 0
+        session["responses"] = []
+        return render_template("home.html")
+    elif session["c_qid"] < len(surveys.satisfaction_survey.questions):
+        flash("Returning you to your unfinished survey")
+        return redirect(f"/question/{session['c_qid']}")
+    #This return will only be reached if user attempts to redo the survey after completing it.
+    return redirect("/finished")
 
 @app.route("/question/<q_number>")
 def show_question_page(q_number):
-    global c_qid
     #check if current question is actually past the last one, then render a different page
     
-    if int(q_number) != c_qid:
-        flask.flash("Invalid question id / Havent completed earlier questions")
-        return flask.redirect(f"/question/{c_qid}")
+    if int(q_number) != session["c_qid"]:
+        flash("Invalid question id / Havent completed earlier questions")
+        return redirect(f"/question/{session['c_qid']}")
     if str(len(surveys.satisfaction_survey.questions)) == q_number:
-            return flask.redirect("/finished")
+        return redirect("/finished")
     c_question = surveys.satisfaction_survey.questions[int(q_number)]
-    return flask.render_template(
+    return render_template(
         "questions.html",
         question_number=str(int(q_number) + 1),
         question=c_question.question,
@@ -38,11 +37,10 @@ def show_question_page(q_number):
 
 @app.route("/answer", methods=["POST"])
 def get_answer():
-    global c_qid
-    responses.append(flask.request.form["answer"])
-    c_qid += 1
-    return flask.redirect(f"/question/{c_qid}")
+    session["responses"].append(request.form["answer"])
+    session["c_qid"] += 1
+    return redirect(f"/question/{session['c_qid']}")
 
 @app.route("/finished")
 def show_finished_page():
-    return flask.render_template("finished.html")
+    return render_template("finished.html")
